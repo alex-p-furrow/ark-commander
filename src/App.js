ca5b2c13984bc7b0a3a466e1205c3fcbfbd45917
Alex Furrow <alex.p.furrow@gmail.com>
2019-11-19 21:21:06 -0500

    Setup material ui.
    Setup a base for material ui.
    Removed uneccessary files from create-react-app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CssBaseline, Card, CardContent, Typography, CardActions, Button } from "@material-ui/core";
+import { createMuiTheme, makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import { blueGrey, pink } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+    palette: {
+        type: "light",
+        primary: blueGrey,
+        secondary: pink
+    }
+});
+
+const useStyles = makeStyles(theme => {
+    return {
+        root: {
+            display: "flex",
+            flex: 1
+        }
+    };
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
+    return (
+        <ThemeProvider theme={theme}>
+            <React.Fragment>
+                <CssBaseline />
+                <Card className={classes.card}>
+                    <CardContent>
+                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                            be
+                            {bull}
+                            nev
+                            {bull}o{bull}
+                            lent
+                        </Typography>
+                        <Typography className={classes.pos} color="textSecondary">
+                            adjective
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            well meaning and kindly.
+                            <br />
+                            {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            </React.Fragment>
+        </ThemeProvider>
+    );
 }
 
 export default App;
