87bb77464a17f1be6b61e4446972ab28760bb2ca
Alex Furrow <alex.p.furrow@gmail.com>
2019-11-26 20:57:06 -0500

    Added search param to get items method.

--- src/utils/arkItems.js
+++ src/utils/arkItems.js
@@ -4,8 +4,13 @@ export function getArkItemCategories() {
     return data.filter((x, i, a) => a.findIndex(z => z.category === x.category) === i).map(y => y.category);
 }
 
-export function getArkItems(category = null) {
-    if (!category) return data;
+export function getArkItems(category = null, searchTerm = null) {
+    if (!category && !searchTerm) return data;
 
-    return data.filter(x => x.category === category);
+    if (category) {
+        const c = data.filter(x => x.category === category);
+        return searchTerm ? c.filter(x => x.name.includes(searchTerm)) : c;
+    }
+
+    return data.filter(x => x.name.includes(searchTerm));
 }
